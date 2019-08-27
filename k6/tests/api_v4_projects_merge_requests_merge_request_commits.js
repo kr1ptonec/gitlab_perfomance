ae445493bc66beea4a838683649dbe5be8f2d107
Grant Young <gyoung@gitlab.com>
2019-08-27 05:55:53 +0000

    Remove nightly n+1
    Just focus on environment tests for now

--- k6/tests/api_v4_projects_merge_requests_merge_request_commits.js
+++ k6/tests/api_v4_projects_merge_requests_merge_request_commits.js
@@ -3,14 +3,23 @@
 import http from "k6/http";
 import { group } from "k6";
 import { Rate } from "k6/metrics";
-import { logError } from "./modules/log_error.js";
+import { logError, getRpsThresholds } from "./modules/custom_k6_modules.js";
 
+// Endpoint is below target threshold. Custom lower limit applied until fixed.
+// Issue: https://gitlab.com/gitlab-org/gitlab-ce/issues/65269
+export let rpsThresholds = getRpsThresholds(0.1)
 export let successRate = new Rate("successful_requests");
 export let options = {
   thresholds: {
     "successful_requests": ["rate>0.95"],
+    "http_reqs": [`count>=${rpsThresholds['count']}`]
   }
 };
+
+export function setup() {
+  console.log('')
+  console.log(`RPS Threshold: ${rpsThresholds['mean']}/s (${rpsThresholds['count']})`)
+}
 
 export default function() {
   group("API - Merge Request Commits", function() {
--- k6/tests/api_v4_projects_repository_branches_branch.js
+++ k6/tests/api_v4_projects_repository_branches_branch.js
@@ -3,16 +3,25 @@
 import http from "k6/http";
 import { group, fail } from "k6";
 import { Rate } from "k6/metrics";
-import { logError } from "./modules/log_error.js";
+import { logError, getRpsThresholds } from "./modules/custom_k6_modules.js";
 
+if (!__ENV.ACCESS_TOKEN) fail('ACCESS_TOKEN has not be set. Exiting...')
+
+// Endpoint is below target threshold. Custom lower limit applied until fixed.
+// Issue: https://gitlab.com/gitlab-org/gitlab-ce/issues/65323
+export let rpsThresholds = getRpsThresholds(0.5)
 export let successRate = new Rate("successful_requests");
 export let options = {
   thresholds: {
     "successful_requests": ["rate>0.95"],
+    "http_reqs": [`count>=${rpsThresholds['count']}`]
   }
 };
 
-if (!__ENV.ACCESS_TOKEN) fail('ACCESS_TOKEN has not be set. Exiting...')
+export function setup() {
+  console.log('')
+  console.log(`RPS Threshold: ${rpsThresholds['mean']}/s (${rpsThresholds['count']})`)
+}
 
 export default function() {
   group("API - Project Repository Branches", function() {
