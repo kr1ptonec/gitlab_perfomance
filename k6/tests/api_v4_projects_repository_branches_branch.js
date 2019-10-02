9199178306da6d41c7a86f842cc29f9d88cfbc99
Grant Young <gyoung@gitlab.com>
2019-10-02 14:09:05 +0000

    Switch k6 tests to measure iterations
    Also fail when no iterations complete by end of test
    
    Was using http_reqs for RPS but this was incorrect (and can also inflate numbers when tests contain more than one request), iterations represent the correct total rps for each test run

--- k6/tests/api_v4_projects_repository_branches_branch.js
+++ k6/tests/api_v4_projects_repository_branches_branch.js
@@ -14,7 +14,7 @@ export let successRate = new Rate("successful_requests");
 export let options = {
   thresholds: {
     "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
-    "http_reqs": [`count>=${rpsThresholds['count']}`]
+    "iterations": [`count>=${rpsThresholds['count']}`]
   }
 };
 
