fdbea3e6af09cf550d44acd19f3567f1b239b042
Nailia Iskhakova <niskhakova@gitlab.com>
2020-01-10 20:45:29 +0300

    Create new group and project for scenario

--- k6/tests/scenarios/scenario_api_unprotect_branch.js
+++ k6/tests/scenarios/scenario_api_unprotect_branch.js
@@ -1,4 +1,4 @@
-/*global __ENV : true  */
+/* global __ENV, __VU, __ITER */
 /*
 @endpoint: `DELETE /projects/:id/protected_branches/:branch`
 @description: [Unprotects the given protected branch or wildcard protected branch](https://docs.gitlab.com/ee/api/protected_branches.html#unprotect-repository-branches)
@@ -7,71 +7,103 @@
 
 import http from "k6/http";
 import { group, fail } from "k6";
-import { Rate } from "k6/metrics";
-import { getRpsThresholds, getProjects, selectProject, adjustRps, adjustStageVUs } from "../../lib/k6_test_modules.js";
+import { Rate, Trend } from "k6/metrics";
+import { logError, getRpsThresholds, adjustRps, adjustStageVUs } from "../../lib/k6_test_modules.js";
 
 if (!__ENV.ACCESS_TOKEN) fail('ACCESS_TOKEN has not been set. Skipping...')
 
+var unprotectTrend = new Trend("unprotect_branch_duration");
+
 // RPS and Success Rate are adjusted to mitigate issues with protect/unprotect request conflitcs
-export let unprotectRps = adjustRps(0.05);
-export let unprotectStages = adjustStageVUs(0.05);
-export let rpsThresholds = getRpsThresholds(0.05);
-export let successThreshold = (__ENV.SUCCESS_RATE_THRESHOLD * 0.7).toFixed(2);
+export let unprotectRps = adjustRps();
+export let unprotectStages = adjustStageVUs();
+export let rpsThresholds = getRpsThresholds();
 export let successRate = new Rate("successful_requests");
 export let options = {
   thresholds: {
-    "successful_requests": [`rate>${successThreshold}`],
+    "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
     "http_reqs": [`count>=${rpsThresholds['count']}`]
   },
   stages: unprotectStages,
   rps: unprotectRps
 };
-export let projects = getProjects(['name', 'group', 'branch']);
 
 export function setup() {
   console.log('')
   console.log(`RPS Threshold: ${rpsThresholds['mean']}/s (${rpsThresholds['count']})`)
-  console.log(`Success Rate Threshold: ${parseFloat(successThreshold)*100}%`)
+  console.log(`Success Rate Threshold: ${parseFloat(__ENV.SUCCESS_RATE_THRESHOLD)*100}%`)
 
-  projects.forEach(function(project) {
-    let protectedBranch = isBranchProtected(project['group'], project['name'], project['branch']);
-    if (protectedBranch) unprotectBranch(project['group'], project['name'], project['branch']);
-  })
+  let groupId = createGroup();
+  let projectId = CreateProject(groupId)
+  let data = { groupId, projectId };
+  return data;
 }
 
-export default function() {
+export default function(data) {
   group("API - Unprotect Protected Branch", function() {
-    let project = selectProject(projects);
-    let protectBranch = {
-      method: "POST",
-      url: `${__ENV.ENVIRONMENT_URL}/api/v4/projects/${project['group']}%2F${project['name']}/protected_branches`,
-      body: { name: project['branch'] },
-      params: { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } }	
-    };
-    let unprotectBranch = {
-      method: "DELETE",
-      url: `${__ENV.ENVIRONMENT_URL}/api/v4/projects/${project['group']}%2F${project['name']}/protected_branches/${project['branch']}`,
-      body: undefined,
-      params: { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } }	
-    };
-    let responses = http.batch( [protectBranch, unprotectBranch] );
-    let unprotectBranchResponse = responses[1];
-    /20(0|4)/.test(unprotectBranchResponse.status) ? successRate.add(true) : successRate.add(false);
+    let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
+
+    let branchName = `protected-test-branch-${__VU}-${__ITER}`
+    console.log(`creating branch ${branchName} in project ${data.projectId}`)
+    let createBranchRes = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${data.projectId}/repository/branches`, { branch: branchName, ref: "master" }, params);
+    /20(0|1|4)/.test(createBranchRes.status) ? successRate.add(true) : successRate.add(false) && logError(createBranchRes);
+
+    console.log(`protecting branch ${branchName} in project ${data.projectId}`)
+    let protectRes = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${data.projectId}/protected_branches`, { name: branchName }, params);
+    /20(0|1|4)/.test(protectRes.status) ? successRate.add(true) : successRate.add(false) && logError(protectRes);
+
+    console.log(`unprotecting branch ${branchName} in project ${data.projectId}`)
+    let unprotectRes = http.del(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${data.projectId}/protected_branches/${branchName}`, undefined, params);
+    /20(0|1|4)/.test(unprotectRes.status) ? successRate.add(true) : successRate.add(false) && logError(unprotectRes);
+    unprotectTrend.add(unprotectRes.timings.duration);
   });
 }
-
-export function teardown() {
-  projects.forEach(project => unprotectBranch(project['group'], project['name'], project['branch']));
+export function teardown(data) {
+  deleteGroup(data.groupId);
 }
 
-function unprotectBranch(groupName,projectName, branch) {
+function createGroup() {
+  let groupName = "group-api-v4-unprotect-branch";
+  let groupId = searchForGroup(groupName);
+  if (groupId) { deleteGroup(groupId) }
+
   let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
-  let res = http.del(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${groupName}%2F${projectName}/protected_branches/${branch}`, undefined, params);
-  return res;
+  let formdata = {
+    name: `${groupName}-${Date.now()}`,
+    path: `${groupName}-${Date.now()}`,
+    visibility: "public"
+  };
+  let res = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/groups`, formdata, params);
+  groupId = JSON.parse(res.body)['id'];
+  /20(0|1)/.test(res.status) ? console.log(`Group #${groupId} was created`) : fail("Group was not created") && logError(res);
+  return groupId;
 }
 
-function isBranchProtected(groupName,projectName, branch) {
+function CreateProject(groupId) {
   let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
-  let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${groupName}%2F${projectName}/protected_branches/${branch}`, params);
-  return /20(0|1)/.test(res.status);
+  let formdata = {
+    name: `project-api-v4-unprotect-branch`,
+    namespace_id: groupId,
+    visibility: "public",
+    initialize_with_readme: true
+  };
+  let res = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/projects`, formdata, params);
+  let projectId = JSON.parse(res.body)['id'];
+  /20(0|1)/.test(res.status) ? console.log(`Project #${projectId} was created`) : fail("Project was not created") && logError(res);
+  return projectId;
+}
+
+function deleteGroup(groupId) {
+  let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
+  let res = http.del(`${__ENV.ENVIRONMENT_URL}/api/v4/groups/${groupId}`, undefined, params);
+  (res.status == "202") ? console.log(`Group #${groupId} was deleted`) : logError(res);
+}
+
+function searchForGroup(groupName) {
+  let params = { headers: { "Accept": "application/json" } };
+  let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/groups?search=${groupName}`, params);
+  let foundGroup = JSON.parse(res.body)[0];
+  let groupId = foundGroup && foundGroup.id;  
+  groupId ? console.log(`Group contaning '${groupName}' name already exists with id=${groupId}`) : console.log(`No groups containing name: '${groupName}'`);
+  return groupId;
 }
