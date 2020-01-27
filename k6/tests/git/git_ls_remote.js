83e5692006869240645efea052d6e581baddb580
Grant Young <gyoung@gitlab.com>
2020-01-27 13:27:34 +0000

    Hotfix git_ls_report to have correct number of VUs

--- k6/tests/git/git_ls_remote.js
+++ k6/tests/git/git_ls_remote.js
@@ -7,9 +7,10 @@
 import http from "k6/http";
 import { group } from "k6";
 import { Rate } from "k6/metrics";
-import { logError, getRpsThresholds, adjustRps, getProjects, selectProject } from "../../lib/gpt_k6_modules.js";
+import { logError, getRpsThresholds, getProjects, selectProject, adjustRps, adjustStageVUs } from "../../lib/gpt_k6_modules.js";
 
 export let gitProtoRps = adjustRps(__ENV.GIT_ENDPOINT_THRESHOLD);
+export let gitProtoStages = adjustStageVUs(__ENV.GIT_ENDPOINT_THRESHOLD);
 export let rpsThresholds = getRpsThresholds(__ENV.GIT_ENDPOINT_THRESHOLD);
 export let successRate = new Rate("successful_requests");
 export let options = {
