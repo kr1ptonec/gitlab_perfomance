4b44b673d9980e64082457e85c19ef9a1d209768
Nailia Iskhakova <niskhakova@gitlab.com>
2020-03-12 15:08:21 +0000

    Expand performance test coverage for issues

---
+++ k6/tests/api/api_v4_projects_issues_issue.js
@@ -0,0 +1,42 @@
+/*global __ENV : true  */
+/*
+@endpoint: `GET /projects/:id/issues/:issue_iid`
+@description: [Get a single project issue](https://docs.gitlab.com/ee/api/issues.html#single-issue)
+*/
+
+import http from "k6/http";
+import { group } from "k6";
+import { Rate } from "k6/metrics";
+import { logError, checkAccessToken, getRpsThresholds, getTtfbThreshold, getProjects, selectProject } from "../../lib/gpt_k6_modules.js";
+
+checkAccessToken();
+
+export let rpsThresholds = getRpsThresholds()
+export let ttfbThreshold = getTtfbThreshold()
+export let successRate = new Rate("successful_requests")
+export let options = {
+  thresholds: {
+    "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
+    "http_req_waiting": [`p(90)<${ttfbThreshold}`],
+    "http_reqs": [`count>=${rpsThresholds['count']}`]
+  }
+};
+
+export let projects = getProjects(['name', 'group', 'issue_iid']);
+
+export function setup() {
+  console.log('')
+  console.log(`RPS Threshold: ${rpsThresholds['mean']}/s (${rpsThresholds['count']})`)
+  console.log(`TTFB P90 Threshold: ${ttfbThreshold}ms`)
+  console.log(`Success Rate Threshold: ${parseFloat(__ENV.SUCCESS_RATE_THRESHOLD)*100}%`)
+}
+
+export default function() {
+  group("API - Issue Overview", function() {
+    let project = selectProject(projects);
+
+    let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
+    let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${project['group']}%2F${project['name']}/issues/${project['issue_iid']}`, params);
+    /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
+  });
+}
