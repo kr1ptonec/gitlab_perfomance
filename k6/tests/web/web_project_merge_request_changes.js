52db10126ff044474dc21227f0a57577c5192968
Nailia Iskhakova <niskhakova@gitlab.com>
2020-03-26 12:18:20 +0000

    Improve table layout on Test Details wiki page

--- k6/tests/web/web_project_merge_request_changes.js
+++ k6/tests/web/web_project_merge_request_changes.js
@@ -2,7 +2,7 @@
 /*
 @endpoint: `GET /:group/:project/merge_requests/:merge_request_iid/diffs`
 @description: Web - Project Merge Request Changes Page. <br>Controllers: `Projects::MergeRequestsController#show`, `Projects::MergeRequests::DiffsController#diffs_metadata.json`, `Projects::MergeRequests::DiffsController#diffs_batch.json`</br>
-@issue: https://gitlab.com/gitlab-org/gitlab/-/issues/209784
+@issue: https://gitlab.com/gitlab-org/gitlab/-/issues/209786
 @gitlab_version: 12.7.0
 */
 
