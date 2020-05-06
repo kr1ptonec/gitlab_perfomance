f1f440a93ede5c5c0137d7ea22ec9b0422f0ee65
Grant Young <gyoung@gitlab.com>
2020-05-06 15:04:45 +0000

    Add complete failures to results score
    No completed iterations tests should still be counted

--- k6/tests/api/api_v4_projects_project_search_blobs.js
+++ k6/tests/api/api_v4_projects_project_search_blobs.js
@@ -9,7 +9,9 @@
 import http from "k6/http";
 import { group } from "k6";
 import { Rate } from "k6/metrics";
-import { logError, getRpsThresholds, getTtfbThreshold, getProjects, selectProject } from "../../lib/gpt_k6_modules.js";
+import { logError, checkAccessToken, getRpsThresholds, getTtfbThreshold, getProjects, selectProject } from "../../lib/gpt_k6_modules.js";
+
+checkAccessToken();
 
 export let rpsThresholds = getRpsThresholds(0.1)
 export let ttfbThreshold = getTtfbThreshold(15000)
