27597197cb6c09f7bf095667a18027f53d4f85ed
Grant Young <gyoung@gitlab.com>
2020-05-29 10:30:06 +0100

    Hotfix web_project_files threshold
    Was set too low.

--- k6/tests/web/web_project_files.js
+++ k6/tests/web/web_project_files.js
@@ -14,7 +14,7 @@ export let endpointCount = 6
 export let webProtoRps = adjustRps(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let webProtoStages = adjustStageVUs(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let rpsThresholds = getRpsThresholds(__ENV.WEB_ENDPOINT_THROUGHPUT, endpointCount)
-export let ttfbThreshold = getTtfbThreshold(1000)
+export let ttfbThreshold = getTtfbThreshold(2000)
 export let successRate = new Rate("successful_requests")
 export let options = {
   thresholds: {
