a4bcaa0a3f0b0bbd5fbd185990c5399b477d1ef7
Grant Young <gyoung@gitlab.com>
2020-06-30 10:49:27 +0000

    Adjust threshold for web_project_commits
    It's failing frequently at just over the threshold. Issue already opened.

--- k6/tests/web/web_project_commits.js
+++ k6/tests/web/web_project_commits.js
@@ -14,7 +14,7 @@ export let endpointCount = 1
 export let webProtoRps = adjustRps(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let webProtoStages = adjustStageVUs(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let rpsThresholds = getRpsThresholds(__ENV.WEB_ENDPOINT_THROUGHPUT, endpointCount)
-export let ttfbThreshold = getTtfbThreshold()
+export let ttfbThreshold = getTtfbThreshold(750)
 export let successRate = new Rate("successful_requests")
 export let options = {
   thresholds: {
