dc44335f62f6282e843a5a13593c568bf3028015
Sanad Liaquat <sliaquat@gitlab.com>
2020-09-15 05:03:09 +0000

    Add test for project variable creation endpoint

--- k6/tests/scenarios/scenario_api_new_group_variables.js
+++ k6/tests/scenarios/scenario_api_new_group_variables.js
@@ -1,7 +1,7 @@
 /* global __ENV, __VU, __ITER */
 /*
-@endpoint: `POST /projects/:id/variables`
-@description: Setup stage: Create group and project <br>Test: [Creates a new project variable](https://docs.gitlab.com/ee/api/project_level_variables.html#create-variable) <br>Teardown stage: Delete group
+@endpoint: `POST /groups/:id/variables`
+@description: Setup stage: Create group <br>Test: [Creates a new group variable](https://docs.gitlab.com/ee/api/group_level_variables.html#create-variable) <br>Teardown stage: Delete group
 @flags: unsafe
 */
 
@@ -9,7 +9,7 @@ import http from "k6/http";
 import { group } from "k6";
 import { Rate } from "k6/metrics";
 import { logError, getRpsThresholds, getTtfbThreshold, adjustRps, adjustStageVUs } from "../../lib/gpt_k6_modules.js";
-import { createGroup, createProject, deleteGroup } from "../../lib/gpt_scenario_functions.js";
+import { createGroup, deleteGroup } from "../../lib/gpt_scenario_functions.js";
 
 export let rps = adjustRps(__ENV.SCENARIO_ENDPOINT_THROUGHPUT)
 export let stages = adjustStageVUs(__ENV.SCENARIO_ENDPOINT_THROUGHPUT)
@@ -21,7 +21,7 @@ export let options = {
     "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
     "http_req_waiting": [`p(90)<${ttfbThreshold}`],
     "http_reqs": [`count>=${rpsThresholds['count']}`],
-    "http_reqs{endpoint:project_variables}": [`count>=${rpsThresholds['count']}`],
+    "http_reqs{endpoint:group_variables}": [`count>=${rpsThresholds['count']}`],
   },
   stages: stages,
   rps: rps
@@ -33,17 +33,16 @@ export function setup() {
   console.log(`TTFB P90 Threshold: ${ttfbThreshold}ms`)
   console.log(`Success Rate Threshold: ${parseFloat(__ENV.SUCCESS_RATE_THRESHOLD)*100}%`)
 
-  let groupId = createGroup("project-api-v4-new-variables");
-  let projectId = createProject(groupId, { builds_access_level: "enabled" });
-  let data = { groupId, projectId };
+  let groupId = createGroup("group-api-v4-new-variables");
+  let data = { groupId };
   return data;
 }
 
 export default function (data) {
-  group("API - Create New Project Variables", function () {
-    let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` }, tags: { endpoint: 'project_variables' } };
+  group("API - Create New Group Variables", function () {
+    let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` }, tags: { endpoint: 'group_variables' } };
     let formdata = { key: `var_key_${__VU}_${__ITER}`, value: `var_value_${__VU}_${__ITER}`, 'protected': false, 'masked': false };
-    let res = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${data.projectId}/variables`, formdata, params);
+    let res = http.post(`${__ENV.ENVIRONMENT_URL}/api/v4/groups/${data.groupId}/variables`, formdata, params);
     /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
   });
 }
@@ -51,5 +50,3 @@ export default function (data) {
 export function teardown(data) {
   deleteGroup(data.groupId);
 }
-
-
