6658b21f7fc2ceab45d46be3a25426de7df5d9b9
Grant Young <gyoung@gitlab.com>
2020-09-28 15:56:10 +0000

    Fixes for files tests
    Large downloads at scale are testing network bandwidth rather than the application

--- k6/tests/api/api_v4_projects_repository_files_file.js
+++ k6/tests/api/api_v4_projects_repository_files_file.js
@@ -34,7 +34,7 @@ export default function() {
     let project = selectRandom(projects);
 
     let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` }, responseType: 'none' };
-    let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${project['group_path_api']}%2F${project['name']}/repository/files/${project['file_source_path']}?ref=master`, params);
+    let res = http.head(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${project['group_path_api']}%2F${project['name']}/repository/files/${project['file_source_path']}?ref=master`, params);
     /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
   });
 }
