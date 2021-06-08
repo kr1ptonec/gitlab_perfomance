293e8e26af234d271a5e7d10e25aec4b95ab31ba
Nailia Iskhakova <niskhakova@gitlab.com>
2021-06-08 22:06:39 +0300

    Add Groups descendant groups API test
    Signed-off-by: Nailia Iskhakova <niskhakova@gitlab.com>

---
+++ k6/tests/api/api_v4_groups_descendant_groups.js
@@ -0,0 +1,38 @@
+/*global __ENV : true  */
+/*
+@endpoint: `GET /groups/:id/descendant_groups`
+@description: [List a group’s descendant groups](https://docs.gitlab.com/ee/api/groups.html#list-a-groups-descendant-groups)
+@gpt_data_version: 1
+@gitlab_version: 13.5.0
+*/
+
+import http from "k6/http";
+import { group } from "k6";
+import { Rate } from "k6/metrics";
+import { logError, getRpsThresholds, getTtfbThreshold } from "../../lib/gpt_k6_modules.js";
+
+export let rpsThresholds = getRpsThresholds()
+export let ttfbThreshold = getTtfbThreshold()
+export let successRate = new Rate("successful_requests")
+export let options = {
+  thresholds: {
+    "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
+    "http_req_waiting": [`p(90)<${ttfbThreshold}`],
+    "http_reqs": [`count>=${rpsThresholds['count']}`]
+  }
+};
+
+export function setup() {
+  console.log('')
+  console.log(`RPS Threshold: ${rpsThresholds['mean']}/s (${rpsThresholds['count']})`)
+  console.log(`TTFB P90 Threshold: ${ttfbThreshold}ms`)
+  console.log(`Success Rate Threshold: ${parseFloat(__ENV.SUCCESS_RATE_THRESHOLD)*100}%`)
+}
+
+export default function() {
+  group("API - Group’s descendant groups", function() {
+    let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
+    let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/groups/${__ENV.ENVIRONMENT_ROOT_GROUP}/descendant_groups`, params);
+    /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
+  });
+}
