de57bc6e3f8143799e583efccb818a0f033f92d9
Nailia Iskhakova <niskhakova@gitlab.com>
2022-07-12 15:46:03 +0000

    Paginate through Web Issue discussions

--- k6/tests/web/web_project_issue.js
+++ k6/tests/web/web_project_issue.js
@@ -11,13 +11,15 @@
 import http from "k6/http";
 import { group } from "k6";
 import { Rate } from "k6/metrics";
-import { logError, getRpsThresholds, getTtfbThreshold, adjustRps, adjustStageVUs, getLargeProjects, selectRandom } from "../../lib/gpt_k6_modules.js";
+import { logError, getRpsThresholds, getTtfbThreshold, adjustRps, adjustStageVUs, getLargeProjects, selectRandom, envVersionIsHigherThan } from "../../lib/gpt_k6_modules.js";
 import { checkProjEndpointDash } from "../../lib/gpt_data_helper_functions.js";
+
+let paginateDiscussions = envVersionIsHigherThan('15.2.0') ? true : false
 
 export let thresholds = {
   'ttfb': { 'latest': 2000 }
 };
-export let endpointCount = 5
+export let endpointCount = paginateDiscussions ? 8 : 5;
 export let webProtoRps = adjustRps(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let webProtoStages = adjustStageVUs(__ENV.WEB_ENDPOINT_THROUGHPUT)
 export let rpsThresholds = getRpsThresholds(__ENV.WEB_ENDPOINT_THROUGHPUT, endpointCount)
@@ -26,17 +28,17 @@ export let successRate = new Rate("successful_requests")
 export let options = {
   thresholds: {
     "successful_requests": [`rate>${__ENV.SUCCESS_RATE_THRESHOLD}`],
-    "http_req_waiting{endpoint:issue}": [`p(90)<${ttfbThreshold}`],
-    "http_req_waiting{endpoint:realtime_changes}": [`p(90)<${ttfbThreshold}`],
-    "http_req_waiting{endpoint:discussions.json}": [`p(90)<${ttfbThreshold}`],
-    "http_req_waiting{endpoint:related_branches}": [`p(90)<${ttfbThreshold}`],
-    "http_req_waiting{endpoint:can_create_branch}": [`p(90)<${ttfbThreshold}`],
+    "http_req_waiting{controller:Projects::IssuesController#show}": [`p(90)<${ttfbThreshold}`],
+    "http_req_waiting{controller:Projects::IssuesController#realtime_changes}": [`p(90)<${ttfbThreshold}`],
+    "http_req_waiting{controller:Projects::IssuesController#discussions.json}": [`p(90)<${ttfbThreshold}`],
+    "http_req_waiting{controller:Projects::IssuesController#related_branches}": [`p(90)<${ttfbThreshold}`],
+    "http_req_waiting{controller:Projects::IssuesController#can_create_branch}": [`p(90)<${ttfbThreshold}`],
     "http_reqs": [`count>=${rpsThresholds['count']}`],
-    "http_reqs{endpoint:issue}": [`count>=${rpsThresholds['count_per_endpoint']}`],
-    "http_reqs{endpoint:realtime_changes}": [`count>=${rpsThresholds['count_per_endpoint']}`],
-    "http_reqs{endpoint:discussions.json}": [`count>=${rpsThresholds['count_per_endpoint']}`],
-    "http_reqs{endpoint:related_branches}": [`count>=${rpsThresholds['count_per_endpoint']}`],
-    "http_reqs{endpoint:can_create_branch}": [`count>=${rpsThresholds['count_per_endpoint']}`]
+    "http_reqs{controller:Projects::IssuesController#show}": [`count>=${rpsThresholds['count_per_endpoint']}`],
+    "http_reqs{controller:Projects::IssuesController#realtime_changes}": [`count>=${rpsThresholds['count_per_endpoint']}`],
+    "http_reqs{controller:Projects::IssuesController#discussions.json}": [`count>=${rpsThresholds['count_per_endpoint']}`],
+    "http_reqs{controller:Projects::IssuesController#related_branches}": [`count>=${rpsThresholds['count_per_endpoint']}`],
+    "http_reqs{controller:Projects::IssuesController#can_create_branch}": [`count>=${rpsThresholds['count_per_endpoint']}`]
   },
   rps: webProtoRps,
   stages: webProtoStages
@@ -64,14 +66,32 @@ export default function(data) {
     let project = selectRandom(projects);
 
     let responses = http.batch([
-      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}`, null, {tags: {endpoint: 'issue', controller: 'Projects::IssuesController', action: 'show'}, redirects: 0}],
-      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/realtime_changes`, null, {tags: {endpoint: 'realtime_changes', controller: 'Projects::IssuesController', action: 'realtime_changes'}, redirects: 0}],
-      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/discussions.json`, null, {tags: {endpoint: 'discussions.json', controller: 'Projects::IssuesController', action: 'discussions.json'}, redirects: 0}],
-      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/related_branches`, null, {tags: {endpoint: 'related_branches', controller: 'Projects::IssuesController', action: 'related_branches'}, headers: { 'Accept': 'application/json' }, redirects: 0}],
-      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/can_create_branch`, null, {tags: {endpoint: 'can_create_branch', controller: 'Projects::IssuesController', action: 'can_create_branch'}, headers: { 'Accept': 'application/json' }, redirects: 0}]
+      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}`, null, {tags: {controller: 'Projects::IssuesController#show'}, redirects: 0}],
+      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/realtime_changes`, null, {tags: {controller: 'Projects::IssuesController#realtime_changes'}, redirects: 0}],
+      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/related_branches`, null, {tags: {controller: 'Projects::IssuesController#related_branches'}, headers: { 'Accept': 'application/json' }, redirects: 0}],
+      ["GET", `${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/can_create_branch`, null, {tags: {controller: 'Projects::IssuesController#can_create_branch'}, headers: { 'Accept': 'application/json' }, redirects: 0}]
     ]);
     responses.forEach(function(res) {
       /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
     });
+
+    // Load in dynamic comments sequentially as page does
+    let pagePaginationBase = 20;
+    let paginateParameter = paginateDiscussions ? `?per_page=${pagePaginationBase}` : "";
+    let discussRes = http.get(`${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/discussions.json${paginateParameter}`, {tags: {controller: 'Projects::IssuesController#discussions.json'}, redirects: 0});
+    /20(0|1)/.test(discussRes.status) ? successRate.add(true) : (successRate.add(false), logError(discussRes));
+
+    if (paginateDiscussions) {
+      // Get all paginated results
+      // https://gitlab.com/gitlab-org/gitlab/-/issues/211377#note_1010122411
+      let nextPageCursor = discussRes.headers['X-Next-Page-Cursor'];
+      let seqDiscussionRes = null;
+      while (nextPageCursor) {
+        pagePaginationBase = Math.ceil(pagePaginationBase * 1.5) // Page sizes: 20, 30, 45, 68, 100
+        seqDiscussionRes = http.get(`${__ENV.ENVIRONMENT_URL}/${project['unencoded_path']}/${data.endpointPath}/${project['issue_iid']}/discussions.json?per_page=${pagePaginationBase}&cursor=${nextPageCursor}`, {tags: {controller: 'Projects::IssuesController#discussions.json'}, redirects: 0});
+        /20(0|1)/.test(seqDiscussionRes.status) ? successRate.add(true) : (successRate.add(false), logError(seqDiscussionRes));
+        nextPageCursor = seqDiscussionRes.headers['X-Next-Page-Cursor'];
+      }
+    }
   });
 }
