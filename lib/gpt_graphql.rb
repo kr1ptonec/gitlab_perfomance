f3407fcdaa5d2dcc3e065c00de4d77024ddc728b
hmuralidhar <hmuralidhar@gitlab.com>
2022-08-11 13:42:03 +0530

    Modified graphql client to support parameterization

--- lib/gpt_graphql.rb
+++ lib/gpt_graphql.rb
@@ -5,23 +5,33 @@ require "graphql/client"
 require "graphql/client/http"
 require 'gpt_logger'
 
-# Initialize graphql client
-module GPTGraphql
-  # Configure GraphQL endpoint using the basic HTTP network adapter
+# Overriding GraphQL::Client::HTTP headers method to pass GITLAB ACCESS_TOKEN
+class CustomGraphqlHttp < GraphQL::Client::HTTP
   GITLAB_TOKEN = ENV.fetch('GITLAB_TOKEN')
-  HTTP = GraphQL::Client::HTTP.new("https://gitlab.com/api/graphql") do
-    def headers(context)
-      { 'PRIVATE-TOKEN' => GITLAB_TOKEN }
-    end
+
+  def headers(context)
+    { 'PRIVATE-TOKEN' => GITLAB_TOKEN }
   end
-
-  SCHEMA = GraphQL::Client.load_schema(HTTP)
-
-  CLIENT = GraphQL::Client.new(schema: SCHEMA, execute: HTTP)
 end
 
-module GetVulnerabilities
-  ProjectQuery = GPTGraphql::Client.parse <<-'GRAPHQL'
+# Create GraphQL client by loading schema and endpoint
+class GQLClient
+  GITLAB_TOKEN = ENV.fetch('GITLAB_TOKEN')
+  def initialize
+    @http = CustomGraphqlHttp.new("https://gitlab.com/api/graphql")
+    @schema = GraphQL::Client.load_schema(@http)
+  end
+
+  def client
+    @client ||= GraphQL::Client.new(schema: @schema, execute: @http)
+  end
+
+end
+
+class Queries
+  # Using Constants for graphql client as recommended here https://github.com/github/graphql-client#defining-queries
+  GQL_CLIENT = GQLClient.new.client
+  GET_QUERY = GQL_CLIENT.parse <<-'GRAPHQL'
      query(
         $path: ID!
          )
@@ -38,10 +48,8 @@ module GetVulnerabilities
           }
         }
   GRAPHQL
-end
 
-module CreateVulnerabilities
-  Mutation = GPTGraphql::Client.parse <<-'GRAPHQL'
+  CREATE_VULNERABILITY_MUTATION = GQL_CLIENT.parse <<-'GRAPHQL'
      mutation(
             $project_id: ProjectID!,
             $name: String!,
@@ -85,51 +93,53 @@ module CreateVulnerabilities
                   }
                }
   GRAPHQL
-end
 
-module MutationParameters
-  def self.name
+  # Defining randomized parameters for vulnerability mutation
+  def name
     SecureRandom.base64(6)
   end
 
-  def self.description
+  def description
     SecureRandom.base64(24)
   end
 
-  def self.mutation_id
+  def mutation_id
     SecureRandom.hex(6)
   end
 
-  def self.scanner_id
+  def scanner_id
     "gid://gitlab/Vulnerabilities::Scanner/#{rand(10000)}"
   end
 
-  def self.scanner_name
+  def scanner_name
     SecureRandom.hex(6)
   end
 
-  def self.severity
+  def severity
     %i[CRITICAL LOW MEDIUM HIGH UNKNOWN INFO].sample
   end
 
-  def self.identifier
+  def identifier
     prefix = %w[CVE CWE].sample
     "#{prefix}-#{SecureRandom.hex(6)}"
   end
-end
 
-module CreateVulnerabilityData
-  def self.create_vulnerability_data(project_id_path:)
-    result = GPTGraphql::Client.query(CreateVulnerabilities::Mutation, variables: { project_id: project_id_path,
-                                                                                    mutation_id: MutationParameters.mutation_id,
-                                                                                    scanner_id: MutationParameters.scanner_id,
-                                                                                    name: MutationParameters.name,
-                                                                                    description: MutationParameters.description,
-                                                                                    scanner_name: MutationParameters.scanner_name,
-                                                                                    identifier: MutationParameters.identifier,
-                                                                                    severity: MutationParameters.severity })
+  def create_vulnerability_data(project_id_path)
+    result = GQL_CLIENT.query(CREATE_VULNERABILITY_MUTATION, variables: { project_id: project_id_path,
+                                                                          mutation_id: mutation_id,
+                                                                          scanner_id: scanner_id,
+                                                                          name: name,
+                                                                          description: description,
+                                                                          scanner_name: scanner_name,
+                                                                          identifier: identifier,
+                                                                          severity: severity })
 
-    GPTLogger.logger.warn "Graphql Error while creating vulnerability data: #{result.data.errors[:vulnerability_create]}" unless result.data.errors.messages.empty?
+    if result.data.nil?
+      GPTLogger.logger.warn "Graphql query Error while creating vulnerability data: #{result.errors[:data]}"
+    elsif result.data.errors.messages
+      GPTLogger.logger.warn "Graphql Server Error while creating vulnerability data: #{result.data.errors[:vulnerability_create]}"
+    end
+
     result
   end
 end
