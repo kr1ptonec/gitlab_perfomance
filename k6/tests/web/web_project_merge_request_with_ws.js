1594fbb41302df9da2e71bfcbfec64562b1a78df
Nailia Iskhakova <niskhakova@gitlab.com>
2022-08-26 16:57:28 +0200

    Move MR with WS calls to a separate test
    Signed-off-by: Nailia Iskhakova <niskhakova@gitlab.com>

--- k6/tests/web/web_project_merge_request_with_ws.js
+++ k6/tests/web/web_project_merge_request_with_ws.js
@@ -1,4 +1,4 @@
-/*global __ENV : true  */
+/*global __ENV, __VU : true  */
 /*
 @endpoint: `GET /:group/:project/merge_requests/:merge_request_iid`
 @example_uri: /:unencoded_path/merge_requests/:mr_discussions_iid
@@ -11,10 +11,13 @@
 */
 
 import http from "k6/http";
-import { group } from "k6";
+import { group, check } from "k6";
+import ws from 'k6/ws';
 import { Rate } from "k6/metrics";
 import { logError, getRpsThresholds, getTtfbThreshold, adjustRps, adjustStageVUs, getLargeProjects, selectRandom } from "../../lib/gpt_k6_modules.js";
 import { checkProjEndpointDash } from "../../lib/gpt_data_helper_functions.js";
+
+const sessionDuration = __ENV.GPT_WS_SESSION_MS || 10000; //10s user session duration
 
 export let thresholds = {
   'rps': { '14.4.0': __ENV.WEB_ENDPOINT_THROUGHPUT * 0.4, 'latest': __ENV.WEB_ENDPOINT_THROUGHPUT },
@@ -41,8 +44,13 @@ export let options = {
     "http_reqs{controller:Projects::MergeRequests::ContentController#cached_widget.json}": [`count>=${rpsThresholds['count_per_endpoint']}`],
     "http_reqs{controller:Projects::MergeRequestsController#show.json}": [`count>=${rpsThresholds['count_per_endpoint']}`],
   },
-  rps: webProtoRps,
-  stages: webProtoStages
+  scenarios: {
+    closed_model: {
+      executor: 'constant-vus',
+      vus: __ENV.GPT_RPS,
+      duration: __ENV.GPT_TEST_DURATION,
+    },
+  },
 };
 
 export let projects = getLargeProjects(['name', 'unencoded_path']);
@@ -59,11 +67,19 @@ export function setup() {
   let checkProject = selectRandom(projects)
   let endpointPath = checkProjEndpointDash(`${__ENV.ENVIRONMENT_URL}/${checkProject['unencoded_path']}`, 'merge_requests')
   console.log(`Endpoint path is '${endpointPath}'`)
-  return { endpointPath };
+
+  let websocketUrl = `ws://${__ENV.ENVIRONMENT_URL.replace(/^https?:\/\//, '')}/-/cable`;
+  console.log(`WebSocket URL is '${websocketUrl}', session duration ${sessionDuration/1000} seconds`)
+
+  let params = { headers: { "Accept": "application/json", "PRIVATE-TOKEN": `${__ENV.ACCESS_TOKEN}` } };
+  let res = http.get(`${__ENV.ENVIRONMENT_URL}/api/v4/projects/${checkProject['encoded_path']}/merge_requests/${checkProject['mr_discussions_iid']}`, params);
+  let mergeRequestID = JSON.parse(res.body)['id'];
+  console.log(`Merge Request ID is '${mergeRequestID}'`)
+  return { endpointPath, websocketUrl, mergeRequestID };
 }
 
 export default function(data) {
-  group("Web - Project Merge Request Page", function() {
+  group("Web - Project Merge Request Page with WebSocket calls", function() {
     let project = selectRandom(projects);
 
     let responses = http.batch([
@@ -76,5 +92,30 @@ export default function(data) {
     responses.forEach(function(res) {
       /20(0|1)/.test(res.status) ? successRate.add(true) : (successRate.add(false), logError(res));
     });
+
+    let params = { headers: { "Origin": `${__ENV.ENVIRONMENT_URL}` } };
+    const res = ws.connect(data.websocketUrl, params, function (socket) {
+      socket.on('open', function open() {
+        console.log(`VU ${__VU}: connected`);
+
+        socket.send(JSON.stringify({ command: 'subscribe', identifier:  `{"channel":"GraphqlChannel","query":"subscription issuableLabelsUpdatedEE($issuableId: IssuableID!) {\\n  issuableLabelsUpdated(issuableId: $issuableId) {\\n    ... on Issue {\\n      id\\n      labels {\\n        nodes {\\n          ...Label\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    ... on MergeRequest {\\n      id\\n      labels {\\n        nodes {\\n          ...Label\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    ... on Epic {\\n      id\\n      labels {\\n        nodes {\\n          ...Label\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment Label on Label {\\n  id\\n  title\\n  description\\n  color\\n  textColor\\n  __typename\\n}\\n","variables":{"issuableId":"gid://gitlab/MergeRequest/${data.mergeRequestID}"},"operationName":"issuableLabelsUpdatedEE","nonce":"8f9548b9-6dd1-4879-9230-551ea264c801"}`}));
+
+      });
+
+      socket.on('message', (data) => console.log('Message received: ', data));
+      socket.on('close', () => console.log('disconnected'));
+
+      socket.setTimeout(function () {
+        console.log(`VU ${__VU}: ${sessionDuration}ms passed, closing MR`);
+        socket.send(JSON.stringify({ event: 'LEAVE' }));
+      }, sessionDuration);
+
+      socket.setTimeout(function () {
+        console.log(`Closing the socket forcefully 3s after graceful LEAVE`);
+        socket.close();
+      }, sessionDuration + 3000);
+    });
+
+    check(res, { 'Connected successfully': (r) => r && r.status === 101 });
   });
 }
